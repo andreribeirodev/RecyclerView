eabb3556aba7e24b7a41f666d3ee8d8d1461d469
andreribeirodev <andreeleeandro@gmail.com>
2021-12-11 11:40:57 -0300

    Evento de click adicionado

--- app/src/main/java/com/example/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.recyclerview.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.RecyclerItemClickListener;
 import com.example.recyclerview.adapter.Adapter;
 import com.example.recyclerview.model.Filme;
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<Filme> listaFilme = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,28 +47,63 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerView.setAdapter(adapter);
 
+        //evento de click
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+
+
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Filme filme = listaFilme.get(position);
+                                Toast.makeText(
+                                        getApplicationContext()
+                                        , "Item pressionado" + filme.getTitulo(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Filme filme = listaFilme.get(position);
+                                Toast.makeText(
+                                        getApplicationContext()
+                                        , "Click longo" + filme.getTitulo(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+
+
     }
-    public void criarFilmes(){
-        Filme filme = new Filme("Homem Aranha - De volta ao lar","Aventura", "2021");
+
+    public void criarFilmes() {
+        Filme filme = new Filme("Homem Aranha - De volta ao lar", "Aventura", "2021");
         this.listaFilme.add(filme);
 
-        filme = new Filme("Mulher Maravilha","Fantasia", "2021");
+        filme = new Filme("Mulher Maravilha", "Fantasia", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("Liga da Justiça","Ficção", "2021");
+        filme = new Filme("Liga da Justiça", "Ficção", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("Capitão América - Guerra Civil","Aventura", "2021");
+        filme = new Filme("Capitão América - Guerra Civil", "Aventura", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("It: A Coisa","Terror", "2021");
+        filme = new Filme("It: A Coisa", "Terror", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("Pica-Pau:O Filme","Comédia", "2021");
+        filme = new Filme("Pica-Pau:O Filme", "Comédia", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("It: A Coisa","Terror", "2021");
+        filme = new Filme("It: A Coisa", "Terror", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("A Bela e a Fera","Romance", "2021");
+        filme = new Filme("A Bela e a Fera", "Romance", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("Meu Malvado favorito 3","Comédia", "2021");
+        filme = new Filme("Meu Malvado favorito 3", "Comédia", "2021");
         this.listaFilme.add(filme);
-        filme = new Filme("Carros 3","Comédia", "2021");
+        filme = new Filme("Carros 3", "Comédia", "2021");
         this.listaFilme.add(filme);
 
     }
